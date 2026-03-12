fb3424ca582d2ef843c0bb4fc8f8b378ac96aa37
K <k@Mac.lan>
2026-03-12 09:48:27 +0200

    refactor: agile chain handling

--- scripts/cmd-open.sh
+++ scripts/cmd-open.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 #Copyright (C) [2014] [Kamen Medarski]
 #This program is free software; you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
@@ -8,64 +9,119 @@
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #GNU General Public License for more details.
 
-n='nft'
-
-#DRY commands
-alias nlcs='$n list chains'
-alias nalc='$n -a list chain inet fw4'
-alias ncc='$n create chain inet fw4'
-alias nlc='$n list chain inet fw4'
-alias nir='$n insert rule inet fw4'
-
-WANCHAIN="input_wan"
-WANFINALRULE="drop_from_wan"
+# --- Config ---
+NFT="nft"
 DEFAULTCHAIN="input_fwknop"
 
-create_unique_rule() {
- if ! ( nalc "$DEFAULTCHAIN" | grep "$1" | grep "$2" | grep "$3" > /dev/null); then
-    nir "$DEFAULTCHAIN" ip saddr "$1" "$2" dport "$3" accept
- fi
+# --- Helpers ---
+
+nft_list()   { $NFT -a  list chain inet fw4 "$1"; }
+nft_listj()  { $NFT -ja list chain inet fw4 "$1"; }
+nft_insert() { $NFT insert rule inet fw4 "$@"; }
+
+log() { echo "$*" >&2; }
+
+# --- Core functions ---
+
+get_iface() {
+    ip -j a | jq -r --arg v "$1" \
+        '.[] | select(any(.addr_info[]; .local == $v)) | .ifname'
 }
 
-ensure_return() {
-  if ! nalc "$DEFAULTCHAIN" | grep "return" > /dev/null; then
-    nir "$DEFAULTCHAIN" return
-  fi
+get_chain() {
+    nft_listj "input" | jq -r --arg v "$1" '
+        .nftables[]
+        | select(
+            .rule.expr? as $expr
+            | $expr and (any($expr[]; .match?["right"]? == $v))
+          )
+        | .rule.expr[1].jump.target? // empty
+    '
 }
 
-create_fwknop_chain() {
-  if ! nlcs | grep "$DEFAULTCHAIN" > /dev/null; then
-  ncc "$DEFAULTCHAIN" > /dev/null
-  fi
+get_first_rule_handle() {
+    nft_listj "$1" | jq -r \
+        '[.nftables[] | select(.rule)] | first | .rule.handle // empty'
 }
 
-case $1 in
-    [0-9]*.[0-9]*.[0-9]*.[0-9]*) IP=$1;;
-    *) echo "Invalid IP address $1";;
-esac
+chain_exists() {
+    $NFT list chains | grep -q "$1"
+}
 
-case $2 in
-    [0-9]*) PORT=$2;;
-    *) echo "Invalid port $2";;
-esac
+chain_has_rule() {
+    nft_list "$1" | grep -q "$2"
+}
 
-case $3 in
-  17) PROTO=udp;;
-  *) PROTO=tcp
-esac
+# Create fwknop chain with a return rule if not exists
+ensure_chain() {
+    if ! chain_exists "$DEFAULTCHAIN"; then
+        log "Creating chain: $DEFAULTCHAIN"
+        $NFT create chain inet fw4 "$DEFAULTCHAIN"
+    fi
+    if ! chain_has_rule "$DEFAULTCHAIN" "return"; then
+        log "Adding return rule to $DEFAULTCHAIN"
+        nft_insert "$DEFAULTCHAIN" return
+    fi
+}
 
-# Handle missing chain. New chain will be created with a simple return statement.
-# Additional rules should be inserted 
+# Insert jump to fwknop chain before first rule of target chain
+ensure_jump() {
+    local ch="$1"
+    if ! chain_has_rule "$ch" "$DEFAULTCHAIN"; then
+        local h
+        h=$(get_first_rule_handle "$ch")
+        log "Inserting jump to $DEFAULTCHAIN in chain $ch before handle $h"
+        nft_insert "$ch" handle "$h" jump "$DEFAULTCHAIN"
+    fi
+}
 
-create_fwknop_chain
-ensure_return
+# Add accept rule if not already present
+ensure_accept_rule() {
+    local ip="$1" proto="$2" port="$3"
+    if ! chain_has_rule "$DEFAULTCHAIN" "$ip" | grep -q "$proto" | grep -q "$port"; then
+        log "Adding rule: $ip $proto $port"
+        nft_insert "$DEFAULTCHAIN" ip saddr "$ip" "$proto" dport "$port" accept
+    fi
+}
 
+# --- Argument parsing ---
 
-# Handle redirect and processing to fwknop chain 
-if ! nlc "$WANCHAIN" | grep "$DEFAULTCHAIN" > /dev/null ; then
-  pos=$(nalc "$WANCHAIN" | grep "$WANFINALRULE" | cut -d'#' -f2 | cut -d' ' -f3)
-  nir "$WANCHAIN" position "$pos" jump "$DEFAULTCHAIN"
-  create_unique_rule "$IP" "$PROTO" "$PORT"
-else
-  create_unique_rule "$IP" "$PROTO" "$PORT"
-fi
+parse_args() {
+    case "$1" in
+        [0-9]*.[0-9]*.[0-9]*.[0-9]*) IP="$1" ;;
+        *) log "Invalid IP: $1"; exit 1 ;;
+    esac
+
+    case "$2" in
+        [0-9]*) PORT="$2" ;;
+        *) log "Invalid port: $2"; exit 1 ;;
+    esac
+
+    case "$3" in
+        17) PROTO="udp" ;;
+        *)  PROTO="tcp" ;;
+    esac
+
+    case "$4" in
+        *.*.*.*:*.*.*.*) SRC_IP="${4%:*}" ;;
+        *) log "Invalid src:dst pair: $4"; exit 1 ;;
+    esac
+}
+
+# --- Main ---
+
+main() {
+    parse_args "$@"
+
+    local iface ch
+    iface=$(get_iface "$SRC_IP")
+    ch=$(get_chain "$iface")
+
+    log "Interface: $iface | Chain: $ch | IP: $IP | Proto: $PROTO | Port: $PORT"
+
+    ensure_chain
+    ensure_jump "$ch"
+    ensure_accept_rule "$IP" "$PROTO" "$PORT"
+}
+
+main "$@"
